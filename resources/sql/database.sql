56a2da7bde38c185f018919e5c1882ffd55fbeb8
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-10-06 17:27:58 -0300

    [feat]: added new table for saving activity

--- resources/sql/database.sql
+++ resources/sql/database.sql
@@ -3,8 +3,16 @@ use duck;
 
 CREATE TABLE users (
     user_id INT auto_increment,
-    username VARCHAR(45),
-    password VARCHAR(45),
+    username VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL,
     PRIMARY KEY (`user_id`)
     );
 
+CREATE TABLE activity_register(
+    registry_id INT auto_increment,
+    user_id INT NOT NULL,
+    document_title VARCHAR(50) NOT NULL,
+    access_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    PRIMARY KEY (`registry_id`)
+);
